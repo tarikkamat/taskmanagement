2733674b836e3c287278349f7bfec2ddf07f71d1
Tarık KAMAT <tarikkamat2@gmail.com>
2025-03-18 22:59:11 +0300

    first commit

---
+++ src/main/java/com/tarikkamat/taskmanagement/common/BaseEntityAudit.java
@@ -0,0 +1,57 @@
+package com.tarikkamat.taskmanagement.common;
+
+import com.tarikkamat.taskmanagement.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class BaseEntityAudit extends BaseEntity implements Serializable {
+    private String createdBy;
+    private String updatedBy;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Date updatedAt;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof BaseEntityAudit that)) return false;
+        if (!super.equals(o)) return false;
+        return createdBy.equals(that.createdBy) &&
+                updatedBy.equals(that.updatedBy) &&
+                createdAt.equals(that.createdAt) &&
+                updatedAt.equals(that.updatedAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(),
+                createdBy, updatedBy, createdAt, updatedAt);
+    }
+
+    @Override
+    public String toString() {
+        return "BaseEntityAudit{" +
+                "createdBy='" + createdBy + '\'' +
+                ", updatedBy='" + updatedBy + '\'' +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                "}" +
+                super.toString();
+    }
+}
