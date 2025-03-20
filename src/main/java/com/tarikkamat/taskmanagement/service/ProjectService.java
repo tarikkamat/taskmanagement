c3cca00c99648b8dced8e02d30e4ab4cc7dc4501
Tarık KAMAT <tarikkamat2@gmail.com>
2025-03-20 05:58:29 +0300

    Add methods to ProjectService for fetching project by ID and converting ProjectDto to entity

--- src/main/java/com/tarikkamat/taskmanagement/service/ProjectService.java
+++ src/main/java/com/tarikkamat/taskmanagement/service/ProjectService.java
@@ -1,9 +1,15 @@
 package com.tarikkamat.taskmanagement.service;
 
 import com.tarikkamat.taskmanagement.dto.ProjectDto;
+import com.tarikkamat.taskmanagement.entity.Project;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface ProjectService {
     List<ProjectDto> getAllProjects();
+
+    ProjectDto getProjectById(UUID id);
+
+    Project toEntity(ProjectDto projectDto);
 }
--- src/main/java/com/tarikkamat/taskmanagement/service/ProjectServiceImpl.java
+++ src/main/java/com/tarikkamat/taskmanagement/service/ProjectServiceImpl.java
@@ -1,6 +1,7 @@
 package com.tarikkamat.taskmanagement.service;
 
 import com.tarikkamat.taskmanagement.dto.ProjectDto;
+import com.tarikkamat.taskmanagement.entity.Project;
 import com.tarikkamat.taskmanagement.entity.User;
 import com.tarikkamat.taskmanagement.mapper.ProjectMapper;
 import com.tarikkamat.taskmanagement.repository.ProjectRepository;
@@ -10,17 +11,22 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class ProjectServiceImpl implements ProjectService {
     private final ProjectRepository projectRepository;
     private final ProjectMapper projectMapper;
-    private final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-    private final User currentUser = (User) authentication.getPrincipal();
+
+    private User getCurrentUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return (User) authentication.getPrincipal();
+    }
 
     @Override
     public List<ProjectDto> getAllProjects() {
+        User currentUser = getCurrentUser();
         List<ProjectDto> projectDtos = projectMapper.toDtoList(projectRepository.findByDepartment_Id(currentUser.getDepartment().getId()));
 
         if (projectDtos.isEmpty()) {
@@ -28,5 +34,16 @@ public class ProjectServiceImpl implements ProjectService {
         }
 
         return projectDtos;
+    }
+
+    @Override
+    public ProjectDto getProjectById(UUID id) {
+        return projectMapper.toDto(projectRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Project not found")));
+    }
+
+    @Override
+    public Project toEntity(ProjectDto projectDto) {
+        return projectMapper.toEntity(projectDto);
     }
 }
