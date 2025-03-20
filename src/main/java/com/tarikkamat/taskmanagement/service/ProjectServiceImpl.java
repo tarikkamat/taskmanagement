a8e7087c015eeb8b390b1a26faf3fb3ea5f9bbfa
Tarık KAMAT <tarikkamat2@gmail.com>
2025-03-20 05:08:27 +0300

    Add method to ProjectRepository for fetching projects by department ID and update ProjectServiceImpl to use it

--- src/main/java/com/tarikkamat/taskmanagement/service/ProjectServiceImpl.java
+++ src/main/java/com/tarikkamat/taskmanagement/service/ProjectServiceImpl.java
@@ -1,9 +1,12 @@
 package com.tarikkamat.taskmanagement.service;
 
 import com.tarikkamat.taskmanagement.dto.ProjectDto;
+import com.tarikkamat.taskmanagement.entity.User;
 import com.tarikkamat.taskmanagement.mapper.ProjectMapper;
 import com.tarikkamat.taskmanagement.repository.ProjectRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,10 +16,12 @@ import java.util.List;
 public class ProjectServiceImpl implements ProjectService {
     private final ProjectRepository projectRepository;
     private final ProjectMapper projectMapper;
+    private final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    private final User currentUser = (User) authentication.getPrincipal();
 
     @Override
     public List<ProjectDto> getAllProjects() {
-        List<ProjectDto> projectDtos = projectMapper.toDtoList(projectRepository.findAll());
+        List<ProjectDto> projectDtos = projectMapper.toDtoList(projectRepository.findByDepartment_Id(currentUser.getDepartment().getId()));
 
         if (projectDtos.isEmpty()) {
             throw new RuntimeException("No projects found");
